de8e68daec2f22fb6099341b14c2e5a62d352874
Sooty <tamas.szekeres0@gmail.com>
2024-02-02 11:16:50 +0100

    F Vechicle class

---
+++ Trucking/Trucking.Vehicle/Vechicle.cs
@@ -0,0 +1,8 @@
+﻿namespace Trucking.Vehicle
+{
+    public class Vechicle
+    {
+        public int Id { get; }
+        public HashSet<string> CompatibleJobs { get; } = new HashSet<string>();
+    }
+}
