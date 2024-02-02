4ba7919625e1f5f9b86d56a673c3dc75b3775c4f
Sooty <tamas.szekeres0@gmail.com>
2024-02-02 12:04:51 +0100

    F job és Vehicle repo

--- Trucking/Trucking.Match/Program.cs
+++ Trucking/Trucking.Match/Program.cs
@@ -1,24 +1,9 @@
-﻿using Trucking.Job;
-using Trucking.Vehicle;
-
-string[] input = File.ReadAllLines(args[0]);
+﻿string[] input = File.ReadAllLines(args[0]);
 
 int numberOfVehicles = int.Parse(input[0]);
-List<Vehicle> vehicles = new List<Vehicle>();
-
-for (int i = 1; i <= numberOfVehicles; i++)
-{
-    var x = input[i].Split(' ');
-    vehicles.Add(new Vehicle(int.Parse(x[0]), x.Skip(1).ToHashSet()));
-}
+VehicleRepository vehicleRepository = new VehicleRepository(input.AsSpan(1,numberOfVehicles));
 
 int numberOfJobs = int.Parse(input[numberOfVehicles + 1]);
-List<Job> jobs = new List<Job>();
-
-for (int i = numberOfVehicles + 2; i < numberOfVehicles + 2 + numberOfJobs; i++)
-{
-    var x = input[i].Split(' ');
-    jobs.Add(new Job(int.Parse(x[0]), x[1]));
-}
+JobRepository jobRepository = new JobRepository(input.AsSpan(numberOfVehicles+2, numberOfJobs));
 
 Console.ReadLine();
