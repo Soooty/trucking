98b59f4fdec5eda507f70c512f5c00c3e831d38c
Sooty <tamas.szekeres0@gmail.com>
2024-02-02 15:10:30 +0100

    F Vehicle Rate propety

--- Trucking/Trucking.Vehicle/Vehicle.cs
+++ Trucking/Trucking.Vehicle/Vehicle.cs
@@ -5,6 +5,7 @@ namespace Trucking.Vehicle
     {
         public int Id { get; }
         public HashSet<string> CompatibleJobs { get; } = new HashSet<string>();
+        public int Rate { get; }
         public bool ReservedForJob { get; private set; }
 
         public Vehicle(int id, HashSet<string> compatibleJobs)
