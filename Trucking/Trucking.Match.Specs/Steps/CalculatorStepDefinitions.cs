6637ac44bae72e8617f041c6f9cb4c53e39831ff
Sooty <tamas.szekeres0@gmail.com>
2024-02-04 17:08:36 +0100

    t első teszt stepjei

---
+++ Trucking/Trucking.Match.Specs/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+using TechTalk.SpecFlow.Assist;
+using Trucking.Job;
+using Trucking.Match.Api;
+using Trucking.Match.Specs.Support;
+using Trucking.Vehicle;
+
+namespace Trucking.Match.Specs.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private VehicleRepository mVehicleRepository;
+        private JobRepository mJobRepository;
+        private Dictionary<int, int> mMatchResult;
+
+        [Given(@"the vehicles")]
+        public void GivenTheVehicles(IEnumerable<IVehicle> vehicles)
+        {
+            mVehicleRepository = new VehicleRepository(vehicles);
+        }
+
+        [Given(@"the jobs")]
+        public void GivenTheJobs(IEnumerable<IJob> jobs)
+        {
+            mJobRepository = new JobRepository(jobs);
+        }
+
+        [When(@"we match the jobs with the vehicles")]
+        public void WhenWeMatchTheJobsWithTheVehicles()
+        {
+            mMatchResult = new JobMatcher.JobMatcher(mVehicleRepository, mJobRepository).Match();
+        }
+
+        [Then(@"following pairs will be fount")]
+        public void ThenFollowingPairsWillBeFount(Table table)
+        {
+            var expected = table.Rows.ToDictionary(
+                r => int.Parse(r["Job id"]), 
+                r => int.Parse(r["Vehicle id"]));
+            Assert.That(mMatchResult, Is.EquivalentTo(expected));
+        }
+
+        [StepArgumentTransformation]
+        public IEnumerable<IVehicle> Vehicles(Table table)
+        {
+            return table.Rows.Select(r=> r.CreateInstance<TestVehicle>()).Cast<IVehicle>();
+        }
+
+        [StepArgumentTransformation]
+        public IEnumerable<IJob> Jobs(Table table)
+        {
+            return table.Rows.Select(r => r.CreateInstance<Job.Job>()).Cast<IJob>();
+        }
+    }
+}
